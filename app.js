2e705cd975fbbe7db7ee62c8cf96b63709198a28
Nazish-Kiran <nazishkirancosmos123@gmail.com>
2024-10-13 21:17:09 +0500

    added gsap animation

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
-const header = document.querySelector("header")
-const menuBtn = document.querySelector("#menu-btn")
-const closeBtn = document.querySelector("#close-btn")
+gsap.from(".stag", {
+  opacity: 0,
+  delay: 1,
+  duration: 2,
+  y: 30,
+  stagger:0.2
+});
 
-menuBtn.addEventListener("click", () =>{
-    header.classList.toggle("show-mobile-menu")
-})
-closeBtn.addEventListener("click", () =>{
-    menuBtn.click();
-})
+const header = document.querySelector("header");
+const menuBtn = document.querySelector("#menu-btn");
+const closeBtn = document.querySelector("#close-btn");
+
+menuBtn.addEventListener("click", () => {
+  header.classList.toggle("show-mobile-menu");
+});
+closeBtn.addEventListener("click", () => {
+  menuBtn.click();
+});
